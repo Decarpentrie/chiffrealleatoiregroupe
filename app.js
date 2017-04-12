76a6a83f95b0839b1aa1667dbe2c7d316e29bf15
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-04-12 11:31:57 +0200

    midification

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-$("#save").click(function() {
-
-	
-	var copains = ["Florian", "Dimitri", "Emilie", "Morel", "Grégory", "Raphael", "Elodie", "Franck", "Jordy", "Marco", "Océane", "Romain", "Mouad", "Maxime", "Julien"];
-	var leCopain;
-
-	for (var i = 0; i<= 14; i ++) {
-		
-		idCopainAleatoire = Math.floor(Math.random() * 4  );
-
-		leCopain = copains.splice( idCopainAleatoire, 1);
-
-		console.log( idCopainAleatoire + '/' + leCopain );
-
-
-	};	
+var personnes = ['Julien', 'Oceane', 'Dinno', 'Emilie', 'Morel', 'Gregory', 'Elodie', 'Romain', 'Franck', 'Marco', 'Rafa', 'Mouad', 'Florian', 'Maxime', 'Jordi'];
 
 
 
+function shuffle(a)
+{
+   var j = 0;
+   var valI = '';
+   var valJ = valI;
+   var l = a.length - 1;
+   while(l > -1)
+   {
+    j = Math.floor(Math.random() * l);
+    valI = a[l];
+    valJ = a[j];
+    a[l] = valJ;
+    a[j] = valI;
+    l = l - 1;
+  }
+  return a;
+ }
 
-	$("tr").append("<tr><td>"++"</td></tr>");
+
+$("#button").click(function(){
+
+  personnes = shuffle(personnes);
+  $(".per").remove();
+  for(var position = personnes.length-1; position >=0; position--){
+    $( $('table tr').get(position+1) ).append('<td class="per">'+personnes[position]+'</td>');
+  }
 
 
+  console.log(personnes);
 
+      
 });
